9bff13a310e2addeaa72cdbbf2bb7d87eaddc991
Mateus Reis <you@example.com>
2022-09-13 15:40:40 -0300

    button new placement

--- resources/js/Pages/Communities/Index.vue
+++ resources/js/Pages/Communities/Index.vue
@@ -13,20 +13,20 @@ defineProps({
     <Head title="All communities" />
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                All communities
-            </h2>
+            <div class="flex items-center justify-between ">
+                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                    All communities
+                </h2>
+                <Link :href="route('communities.create')">
+                <PrimaryButton>
+                    New
+                </PrimaryButton>
+                </Link>
+            </div>
         </template>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="flex items-center justify-end mt-4">
-                    <Link :href="route('communities.create')">
-                    <PrimaryButton class="ml-4">
-                        New
-                    </PrimaryButton>
-                    </Link>
-                </div>
                 <div class="overflow-x-auto relative">
                     <table class="w-full text-base text-left text-gray-500 dark:text-gray-400">
                         <thead class="text-sm text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
@@ -52,8 +52,9 @@ defineProps({
                                     {{community.slug}}
                                 </td>
                                 <td class="py-4 px-6">
-                                    <Link class="font-medium text-blue-600 dark:text-blue-500 hover:underline" :href="route('communities.edit', community.id)">
-                                        Edit
+                                    <Link class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                                        :href="route('communities.edit', community.id)">
+                                    Edit
                                     </Link>
                                 </td>
                             </tr>
