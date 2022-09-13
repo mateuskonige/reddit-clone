c4578b991778258f0203aadc98aeac9e91c20367
Mateus Reis <you@example.com>
2022-09-13 14:59:42 -0300

    communities index

---
+++ resources/js/Pages/Communities/Index.vue
@@ -0,0 +1,64 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import PrimaryButton from '@/Components/PrimaryButton.vue';
+import { Head, Link } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    communities: Array,
+})
+</script>
+
+<template>
+
+    <Head title="All communities" />
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                All communities
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="flex items-center justify-end mt-4">
+                    <Link :href="route('communities.create')">
+                    <PrimaryButton class="ml-4">
+                        New
+                    </PrimaryButton>
+                    </Link>
+                </div>
+                <div class="overflow-x-auto relative">
+                    <table class="w-full text-base text-left text-gray-500 dark:text-gray-400">
+                        <thead class="text-sm text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="py-3 px-6">
+                                    Name
+                                </th>
+                                <th scope="col" class="py-3 px-6">
+                                    Slug
+                                </th>
+                                <th scope="col" class="py-3 px-6">
+                                    Actions
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody v-for="community in communities" :key="community.id">
+                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <th scope="row"
+                                    class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{community.name}}
+                                </th>
+                                <td class="py-4 px-6">
+                                    {{community.slug}}
+                                </td>
+                                <td class="py-4 px-6">
+                                    Edit
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
